b02f0a7cd73dae2bce23f7ce90cdec58c9b75a84
pcaridade/H <paulocaridade@gmail.com>
2018-02-24 18:19:54 +0000

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,12 @@ var socket = io();
 
 socket.on('connect', function () {
     console.log('Connected to server');
-
+/*
     socket.emit('createMessage', { 
         from: 'Andrew',
         text: 'Yup, that works for me'
     });
+*/    
 });
 
 socket.on('disconnect', function () {
--- server/server.js
+++ server/server.js
@@ -15,14 +15,22 @@ app.use(express.static(publicpath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
+    /*
     socket.emit('newMessage', {
         from: 'John',
         text: "See you then",
         createdAt: 123123
     });
+    */
 
     socket.on('createMessage', (message)=>{
         console.log('createMessage', message);
+
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        })
     });
 
     socket.on('disconnect', ()=>{
